841d799419af7ee8a1e2bd3e481e17302bf5792d
Gogs <gogs@fake.local>
2019-02-26 16:06:31 +0800

    Deployed 9fb5af5 with MkDocs version: 1.0.4

---
+++ index.md.mk
@@ -0,0 +1,76 @@
+
+## 联系方式
+- QQ：610658552
+- 微信:  610658552
+- Email：610658552@qq.com
+
+## 个人信息
+ -  蔡猛芝
+ - 本科/南京工业大学
+ - 工作年限：8年
+ - 技术博客： https://caimengzhi.github.io/books
+ - 期望职位：运维开发，Linux运维
+ - 期望薪资：税前月薪20k，特别喜欢的公司可例外
+ - 期望城市：南京  徐州  宿迁
+
+## 工作经历
+### 南京真云（ 2015年5月 ~ 至今 ）
+
+#### 1. 日志爬虫 
+我在此项目负责了哪些工作，分别在哪些地方做得出色/和别人不一样/成长快，这个项目中，我最困难的问题是什么，我采取了什么措施，最后结果如何。这个项目中，我最自豪的技术细节是什么，为什么，实施前和实施后的数据对比如何，同事和领导对此的反应如何。
+
+#### 2. 自动部署
+- 基于ansible+django的自动化部署
+- 独立完成整套前后端
+
+#### 3. 资产管理
+- 基于django + ansible 管理IDC机房服务软件硬件
+- 后端开发+部分前端开发
+
+#### 4. IDC/办公
+- 负责公司内部办公网络，设备维护
+- IDC机房服务器，防火墙，交换机等设备软件和硬件维护
+
+### 南京华为（  2014.4 ~2015.5 ）
+#### 1. 巡检
+- 华为IPTV项目组的巡检维护助手工具（mainast）的开发与维护，支持全球一线测试人员技术指导
+- 根据项目组的开发需求，着实修改和测试巡检
+
+#### 2. 自动化平台
+- 基于shell和python脚本的华为自动化web部署工具ideploy的二次开发
+- 主要写shell和python脚本
+
+### 江苏捷世智通  ( ~ 2015/4)
+-  嵌入式软件硬件设计
+-  结合intel，国产CPU 龙芯LX43A根据客户要求，设计生产嵌入式板卡
+-  设计调试28所军方手持pad，炮兵雷达发射装置，步兵加固笔记本，工业计算机等计算机软硬件
+-  原理图的设计，硬件PCB的策划与实施，生产跟踪指导，验收测试，软件的项目实施与验证实施
+-  Linux驱动修改编译，内核裁剪，部署安装等等       
+## 技能清单
+以下均为我熟练使用的技能
+
+-  Web开发：Python
+-  Web框架：Django/Flask
+-  前端框架：Bootstrap/HTML5/Vue
+-  数据库相关：MySQL/Redis/Memcache/Mongodb
+-  版本管理、文档和自动化部署工具：Svn/Git
+-  单元测试：SimpleTest/Qunit
+-  基础服务: apache，nginx，NFS，ftp，samba,srsync,sersync，intoify etc
+-  分布式存储: mfs/fastdfs
+-  自动工具: ansible
+-  负载均衡: nginx/lvs/haproxy
+-  数据库高可用mysql-mha  redis-sentinel etc
+-  虚拟化: kvm/vmware/docker
+-  备份: mysql(全量/增量) /log/data
+-  监控: nagios/zabbix/telegraf+influxdb+grafana
+-  防火墙: 山石(硬件)  /iptables(软件)
+-  系统: centos/redhat/ubuntu
+-  画图:精通使用visio,亿图画专业服务器网络图
+-  vpn: 熟练openvpn，以及跨机房加密隧道通讯，熟练squid反代和正代，nginx代理等
+-  流媒体:nginx rtmp,m3u8直播
+-  精通嵌入式软件和硬件（军方嵌入式4年工作经验）
+  
+---      
+## 致谢
+感谢您花时间阅读我的简历，期待能有机会和您共事。
+      
